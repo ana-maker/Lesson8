9cc45a81e6c9feb02a32ea2cbf89dbf6ab2f001f
Ana Vornic <anavornic1996@gmail.com>
2020-02-17 13:47:40 +0200

    Lesson 9

--- controller/photoGallery.php
+++ controller/photoGallery.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../autoload.php';
 use App\Models\View;
 
 $view = new View();
-$namedir = './../images';
+$namedir = __DIR__ .'/../images';
 $dir = scandir($namedir);
 $view->assign('dir', $dir);
 $view->display(__DIR__ . '/../templates/photo_gallery_template.php');
